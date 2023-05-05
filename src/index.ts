a24cb5de37eeabc6d854228b874adaf62459e13b
Lean Mendoza <leandro@decentraland.org>
2023-05-04 21:17:32 -0300

    use @next and remove temp code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-
-// TEMPORARY UNTIL @dcl/explorer is up-to-date 
-import engineApi from '~system/EngineApi'
-const originalCrdtGetState = engineApi.crdtGetState
-engineApi.crdtGetState = async (body: any): Promise<any> => ({...originalCrdtGetState(body), hasEntities: false})
-
 // Generate main entry point
 import { executeTask } from '@dcl/sdk/ecs'
 import { main } from './main'
